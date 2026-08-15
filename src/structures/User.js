ca57e2296e37ef4aa737453d2796883ce68e45ae
Yi Jun <yijun@thedu.cc>
2026-08-15 18:28:25 +0800

    v0.1.1 - status in User object

--- src/structures/User.js
+++ src/structures/User.js
@@ -10,7 +10,6 @@ class User extends Base {
   _patch(data) {
     if ('username' in data) this.username = data.username;
     if ('avatarUrl' in data) this.avatarUrl = data.avatarUrl;
-    if ('status' in data) this.status = data.status;
     return this;
   }
 
